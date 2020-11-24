40f058cf02264fe6d88df6ab3abce296dfa29eac
Книга Рецептов <shukenguken@gmail.com>
2020-11-24 18:33:40 +0700

    AAAAAAAAAAAAAAAAAAA

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="shrift">
+     <div className="header"><i>Вы зашли как Имя_аналитика</i></div>
+     <div className="sidebar">
+        <b>Проблемы</b>
+        <p>Проблема 1</p><p>Проблема 2</p>
+        <p>Проблема 3</p><p>Проблема 4</p>
+        <p>Проблема 5</p><p>Проблема 6</p>
+     </div>
+     <div className="content">
+        <h2>Формулировка проблемы</h2>
+        <p>Формулировка проблемы.</p>
+     </div>
+    </body>
   );
 }
+
+// class Hello extends React.Component {
+//     render() {
+//         return <h1>Hello</h1>;
+//     }
+// }
 
 export default App;
