f2eb97eabb1468cf0e74db13885ae6b9f3e55049
toqich <shukenguken@gmail.com>
2020-12-26 15:07:41 +0700

    АА 5 лр аналитик

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <tbody>
           <tr>
             <td bgcolor="#f1f1f1"><textarea className="norm_text" style={{ height: 20, width: 100, resize: "none" }}
-                                            disabled="true">Транспорт</textarea></td>
+                                            disabled="true">Студенты</textarea></td>
             <td>
               <button style={{ marginLeft: "3px" }}>&#9998;</button>
             </td>
@@ -62,14 +62,14 @@ function App() {
     </div>
     <div className="content">
       <tr>
-        <td><h3>Формулировка проблемы Транспорт</h3></td>
+        <td><h3>Формулировка проблемы Студенты</h3></td>
         <td>
           <button style={{ marginLeft: "655px" }}>Сохранить изменения</button>
         </td>
         <td></td>
       </tr>
       <p></p>
-      <textarea className="norm_text" style={{ height: 40, width: 1100, resize: "none" }} disabled="true">Решение транспортной проблемы в густозаселенном новом районе города.</textarea>
+      <textarea className="norm_text" style={{ height: 40, width: 1100, resize: "none" }}>Проблема развития студенческого самоуправления означает проблему подготовки студента как гражданина, способного участвовать в управлении, принимать грамотные решения и эффективно выполнять эти решения на рабочем месте.</textarea>
       <p></p>
       <div class="tabs">
         <label style={{ fontSize: "13px" }} className={tabs1 === 1 ? "active_label" : ""}
@@ -80,7 +80,7 @@ function App() {
                onClick={() => setTabs1(3)}>Результаты</label>
         {tabs1 === 1 &&
         <div id="txt_3">
-          <button disabled="true">Добавить альтернативу</button>
+          <button>Добавить альтернативу</button>
           <table>
             <tr>
               <th>№</th>
@@ -89,37 +89,37 @@ function App() {
             <tbody>
             <tr>
               <td>1.</td>
-              <td><textarea className="norm_text" disabled="true">Строительство новой ветки метро</textarea></td>
+              <td><textarea className="norm_text">Проведение тренингов по развитию руководственных навыков</textarea></td>
               <td>
-                <button style={{ marginLeft: "38px" }} disabled="true">&#10008;</button>
+                <button style={{ marginLeft: "38px" }}>&#10008;</button>
               </td>
             </tr>
             <tr>
               <td>2.</td>
-              <td><textarea className="norm_text" disabled="true">Расширение парка муниципальных автобусов</textarea></td>
+              <td><textarea className="norm_text">Ввести контроль и отчетность перед администрацией учебного подразделения</textarea></td>
               <td>
-                <button style={{ marginLeft: "38px" }}  disabled="true">&#10008;</button>
+                <button style={{ marginLeft: "38px" }}>&#10008;</button>
               </td>
             </tr>
             <tr>
               <td>3.</td>
-              <td><textarea className="norm_text"  disabled="true">Расширение транспортной сети</textarea></td>
+              <td><textarea className="norm_text">Повысить эффективность каналов передачи информации</textarea></td>
               <td>
-                <button style={{ marginLeft: "38px" }}  disabled="true">&#10008;</button>
+                <button style={{ marginLeft: "38px" }}>&#10008;</button>
               </td>
             </tr>
             <tr>
               <td>4.</td>
-              <td><textarea className="norm_text" disabled="true">Ввод скоростного трамвая</textarea></td>
+              <td><textarea className="norm_text">Укрепить нормативно-правовую базу студенческого самоуправления</textarea></td>
               <td>
-                <button style={{ marginLeft: "38px" }}  disabled="true">&#10008;</button>
+                <button style={{ marginLeft: "38px" }}>&#10008;</button>
               </td>
             </tr>
             <tr>
               <td>5.</td>
-              <td><textarea className="norm_text" disabled="true">Оптимизация структуры движения городских маршрутов</textarea></td>
+              <td><textarea className="norm_text">Провести анкетирование удовлетворенности студенческого самоуправления</textarea></td>
               <td>
-                <button style={{ marginLeft: "38px" }}  disabled="true">&#10008;</button>
+                <button style={{ marginLeft: "38px" }}>&#10008;</button>
               </td>
             </tr>
             </tbody>
@@ -133,8 +133,7 @@ function App() {
               <label style={{ fontSize: "13px" }} className={tabs2 === 1 ? "active_label" : ""}
                      onClick={() => setTabs2(1)}>Назначенные</label>
               <label style={{ fontSize: "13px" }} className={tabs2 === 2 ? "active_label" : ""}
-                     onClick={() => setTabs2(2)}>Все
-                эксперты</label>
+                     onClick={() => setTabs2(2)}>Все эксперты</label>
               <button style={{ marginLeft: "25px" }}>Добавить эксперта</button>
               <p></p>
               {tabs2 === 1 &&
@@ -168,30 +167,37 @@ function App() {
                   <tbody>
                   <tr>
                     <td><textarea className="norm_text_expert">Эксперт 1</textarea></td>
-                    <td><textarea className="norm_text_expert" style={{width: "200px", height: "32px"}}>Главный архитектор города</textarea></td>
-                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>30</textarea></td>
-                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>8</textarea></td>
-                    <td>Метод парных оценок(100%)<br/>Метод взвешенных оценок(60%)</td>
-                    <td><input type="checkbox" style={{ marginLeft: "38px" }} checked="checked" disabled="true"></input></td>
+                    <td><textarea className="norm_text_expert" style={{width: "200px", height: "32px"}}>Студент</textarea></td>
+                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>0</textarea></td>
+                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>5</textarea></td>
+                    <td>Метод парных оценок(0%)<br/>Метод взвешенных оценок(0%)<br/>Метод преподчения(0%)<br/>Метод ранга(0%)<br/>Метод полного попарного сравнения(0%)</td>
+                    <td><input type="checkbox" style={{ marginLeft: "38px" }} checked="checked"></input></td>
                   </tr>
                   <tr>
                     <td><textarea className="norm_text_expert">Эксперт 2</textarea></td>
-                    <td><textarea className="norm_text_expert" style={{width: "200px", height: "32px"}}>Председатель комитета по градоустройству</textarea></td>
-                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>15</textarea></td>
-                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>6.5</textarea></td>
-                    <td>Метод парных оценок(0%)<br/>Метод взвешенных оценок(0%)</td>
+                    <td><textarea className="norm_text_expert" style={{width: "200px", height: "32px"}}>Студент</textarea></td>
+                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>0</textarea></td>
+                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>5</textarea></td>
+                    <td>Метод парных оценок(0%)<br/>Метод взвешенных оценок(0%)<br/>Метод преподчения(0%)<br/>Метод ранга(0%)<br/>Метод полного попарного сравнения(0%)</td>
                     <td><input type="checkbox" style={{ marginLeft: "38px" }}></input></td>
                   </tr>
-
+                  <tr>
+                    <td><textarea className="norm_text_expert">Эксперт 3</textarea></td>
+                    <td><textarea className="norm_text_expert" style={{width: "200px", height: "32px"}}>Студент</textarea></td>
+                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>0</textarea></td>
+                    <td><textarea className="norm_text_expert" style={{width: "30px"}}>5</textarea></td>
+                    <td>Метод парных оценок(0%)<br/>Метод взвешенных оценок(0%)<br/>Метод преподчения(0%)<br/>Метод ранга(0%)<br/>Метод полного попарного сравнения(0%)</td>
+                    <td><input type="checkbox" style={{ marginLeft: "38px" }}></input></td>
+                  </tr>
                   </tbody>
                 </table>
 
                 <div>
                   <td>
-                    <button disabled="true">Разрешить оценивание проблемы</button>
+                    <button>Разрешить оценивание проблемы</button>
                   </td>
                   <td>
-                    <button style={{ marginLeft: "710px" }} disabled="true">Удалить эксперта</button>
+                    <button style={{ marginLeft: "710px" }}>Удалить эксперта</button>
                   </td>
                 </div>
               </>
@@ -206,87 +212,208 @@ function App() {
           <option disabled>Выберите эксперта</option>
           <option>Эксперт 1</option>
           <option>Эксперт 2</option>
+          <option>Эксперт 3</option>
         </select><br/>
-          <b>Метод парных сравнений</b>
-          <tr><td>
-              </td>
-              <td style={{ marginLeft: "10px" }}>
-              <table text-align="left" border="1px">
-                <tr>
-                  <th width="5px">Ранг</th>
-                  <th width="10px">Вес</th>
-                  <th width="40px">Альтернатива</th>
-                </tr>
-                <tbody>
-                <tr>
-                  <td height="16px" width="20px">1.</td>
-                  <td height="16px" >0.45</td>
-                  <td height="16px">Строительство новой ветки метро</td>
-                </tr>
-                <tr>
-                  <td>2.</td>
-                  <td>0.218</td>
-                  <td>Расширение парка муниципальных автобусов</td>
-                </tr>
-                <tr>
-                  <td>3.</td>
-                  <td>0.166</td>
-                  <td>Оптимизация структуры движения городских маршрутов</td>
-                </tr>
-                <tr>
-                  <td>3.</td>
-                  <td>0.166</td>
-                  <td>Ввод скоростного трамвая</td>
-                </tr>
-                <tr>
-                  <td>4.</td>
-                  <td>0</td>
-                  <td>Расширение транспортной сети</td>
-                </tr>
-                </tbody>
-              </table>
-              </td>
-            </tr>
+        <b>Метод парных сравнений</b>
+        <tr><td>
+            </td>
+            <td style={{ marginLeft: "10px" }}>
+            <table text-align="left" border="1px">
+              <tr>
+              <th style={{ width: "20px" }}>Ранг</th>
+              <th style={{ width: "10px" }}>Вес</th>
+              <th style={{ width: "500px" }}>Альтернатива</th>
+              </tr>
+              <tbody>
+              <tr>
+                <td>1.</td>
+                <td>0.33</td>
+                <td>Укрепить нормативно-правовую базу студенческого самоуправления</td>
+              </tr>
+              <tr>
+                <td>2.</td>
+                <td>0.22</td>
+                <td>Контроль и отчетность перед администрацией<br/>учебного подразделения</td>
+              </tr>
+              <tr>
+                <td>3.</td>
+                <td>0.21</td>
+                <td>Провести анкетирование удовлетворенности<br/>студенческого самоуправления</td>
+              </tr>
+              <tr>
+                <td>4.</td>
+                <td>0.19</td>
+                <td>Повысить эффективность каналов передачи<br/>информации</td>
+              </tr>
+              <tr>
+                <td>5.</td>
+                <td>0.05</td>
+                <td>Проведение тренингов по развитию<br/>руководственных навыков</td>
+              </tr>
+              </tbody>
+            </table>
+            </td>
+          </tr>
             <b>Метод взвешенных оценок</b>
             <tr><td>
                 </td>
                 <td style={{ marginLeft: "10px" }}>
                 <table text-align="left" border="1px">
                   <tr>
-                    <th width="5px">Ранг</th>
-                    <th width="10px">Вес</th>
-                    <th width="40px">Альтернатива</th>
+                  <th style={{ width: "20px" }}>Ранг</th>
+                  <th style={{ width: "10px" }}>Вес</th>
+                  <th style={{ width: "500px" }}>Альтернатива</th>
                   </tr>
                   <tbody>
                   <tr>
-                    <td height="16px" width="20px">1.</td>
-                    <td height="16px" >0.3</td>
-                    <td height="16px">Ввод скоростного трамвая</td>
+                    <td>1.</td>
+                    <td>0.35</td>
+                    <td>Провести анкетирование удовлетворенности<br/>студенческого самоуправления</td>
                   </tr>
                   <tr>
                     <td>2.</td>
                     <td>0.2</td>
-                    <td>Строительство новой ветки метро</td>
+                    <td>Укрепить нормативно-правовую базу студенческого самоуправления</td>
                   </tr>
                   <tr>
                     <td>2.</td>
                     <td>0.2</td>
-                    <td>Расширение транспортной сети</td>
+                    <td>Повысить эффективность каналов передачи<br/>информации</td>
                   </tr>
                   <tr>
                     <td>3.</td>
-                    <td>0.15</td>
-                    <td>Расширение парка муниципальных автобусов</td>
+                    <td>0.17</td>
+                    <td>Контроль и отчетность перед администрацией<br/>учебного подразделения</td>
                   </tr>
                   <tr>
-                    <td>3.</td>
-                    <td>0.15</td>
-                    <td>Оптимизация структуры движения городских маршрутов</td>
+                    <td>4.</td>
+                    <td>0.8</td>
+                    <td>Проведение тренингов по развитию<br/>руководственных навыков</td>
                   </tr>
                   </tbody>
                 </table>
                 </td>
               </tr>
+              <b>Метод предпочтения</b>
+              <tr><td>
+                  </td>
+                  <td style={{ marginLeft: "10px" }}>
+                  <table text-align="left" border="1px">
+                    <tr>
+                    <th style={{ width: "20px" }}>Ранг</th>
+                    <th style={{ width: "10px" }}>Вес</th>
+                    <th style={{ width: "500px" }}>Альтернатива</th>
+                    </tr>
+                  <tbody>
+                  <tr>
+                    <td>1.</td>
+                    <td>0.3</td>
+                    <td>Укрепить нормативно-правовую базу студенческого самоуправления</td>
+                  </tr>
+                  <tr>
+                    <td>2.</td>
+                    <td>0.225</td>
+                    <td>Контроль и отчетность перед администрацией<br/>учебного подразделения</td>
+                  </tr>
+                  <tr>
+                    <td>3.</td>
+                    <td>0.205</td>
+                    <td>Провести анкетирование удовлетворенности<br/>студенческого самоуправления</td>
+                  </tr>
+                  <tr>
+                    <td>4.</td>
+                    <td>0.15</td>
+                    <td>Проведение тренингов по развитию<br/>руководственных навыков</td>
+                  </tr>
+                  <tr>
+                    <td>4.</td>
+                    <td>0.12</td>
+                    <td>Повысить эффективность каналов передачи<br/>информации</td>
+                  </tr>
+                  </tbody>
+                </table>
+                  </td>
+                </tr>
+                <b>Метод ранга</b>
+                <tr><td>
+                    </td>
+                    <td style={{ marginLeft: "10px" }}>
+                    <table text-align="left" border="1px">
+                      <tr>
+                      <th style={{ width: "20px" }}>Ранг</th>
+                      <th style={{ width: "10px" }}>Вес</th>
+                      <th style={{ width: "500px" }}>Альтернатива</th>
+                      </tr>
+                    <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>0.3</td>
+                      <td>Укрепить нормативно-правовую базу студенческого самоуправления</td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>0.25</td>
+                      <td>Провести анкетирование удовлетворенности<br/>студенческого самоуправления</td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>0.25</td>
+                      <td>Контроль и отчетность перед администрацией<br/>учебного подразделения</td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>0.15</td>
+                      <td>Проведение тренингов по развитию<br/>руководственных навыков</td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>0.05</td>
+                      <td>Повысить эффективность каналов передачи<br/>информации</td>
+                    </tr>
+                    </tbody>
+                  </table>
+                    </td>
+                  </tr>
+                  <b>Метод полного попарного сопоставления</b>
+                  <tr><td>
+                      </td>
+                      <td style={{ marginLeft: "10px" }}>
+                      <table text-align="left" border="1px">
+                        <tr>
+                          <th style={{ width: "20px" }}>Ранг</th>
+                          <th style={{ width: "10px" }}>Вес</th>
+                          <th style={{ width: "500px" }}>Альтернатива</th>
+                        </tr>
+                        <tbody>
+                        <tr>
+                          <td>1.</td>
+                          <td>0.34</td>
+                          <td>Укрепить нормативно-правовую базу студенческого самоуправления</td>
+                        </tr>
+                        <tr>
+                          <td>2.</td>
+                          <td>0.21</td>
+                          <td>Провести анкетирование удовлетворенности<br/>студенческого самоуправления</td>
+                        </tr>
+                        <tr>
+                          <td>3.</td>
+                          <td>0.2</td>
+                          <td>Контроль и отчетность перед администрацией<br/>учебного подразделения</td>
+                        </tr>
+                        <tr>
+                          <td>4.</td>
+                          <td>0.18</td>
+                          <td>Проведение тренингов по развитию<br/>руководственных навыков</td>
+                        </tr>
+                        <tr>
+                          <td>5.</td>
+                          <td>0.07</td>
+                          <td>Повысить эффективность каналов передачи<br/>информации</td>
+                        </tr>
+                        </tbody>
+                      </table>
+                      </td>
+                    </tr>
           <button >Отметить проблему как решенную</button>
         </div>
         }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
-// import Expert from './Expert'
+import Expert from './Expert'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <App/>
+    <Expert/>
   </React.StrictMode>,
   document.getElementById('root')
 );
